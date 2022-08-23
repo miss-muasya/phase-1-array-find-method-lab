22bed0eac376661005c13f4db6e8c7c4cd6cecc0
miss-muasya <sheemuasya@gmail.com>
2022-08-23 15:51:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+    const result = record.find(record => record.result === "W");
+    if(!result){
+      return undefined
+    }
+    return result.year
+  }
